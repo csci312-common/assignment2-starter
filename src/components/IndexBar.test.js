f7c161bc908a664df76323cf7fe410bb83b03839
Christopher Andrews <candrews@middlebury.edu>
2019-09-29 23:00:30 -0400

    Further migration to Immutable.

--- src/components/IndexBar.test.js
+++ src/components/IndexBar.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import Immutable from 'immutable';
 
 import { mapFromArticleList } from '../App';
 import IndexBar from './IndexBar';
@@ -36,7 +37,7 @@ const collection = mapFromArticleList(articles);
 
 describe('IndexBar initialization', () => {
   test('Handles empty array without error', () => {
-    shallow(<IndexBar collection={new Map()} select={jest.fn} />);
+    shallow(<IndexBar collection={Immutable.Map} select={jest.fn} />);
   });
 });
 
